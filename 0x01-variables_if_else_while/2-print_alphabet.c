d28bc8fbf02533e810e87b004303ac2f014e251a
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-14 16:42:33 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,14 @@
 /**
  * main - Entry point
  *
- * Return; Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <='z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	putchar('\n');
 
