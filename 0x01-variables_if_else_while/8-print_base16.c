c1dee819cf2c5b26e8da096e39d3afe8e614f529
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-14 16:08:36 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: main - start
+ *
+ * Return 1 failure
+ */
+
+int main(voi)
+{
+	for (char i = 0; i < 10; i++)
+		putchar(i);
+	for (int j = 97; j < 103; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
