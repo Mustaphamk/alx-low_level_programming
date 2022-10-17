2fcfc90991d4c6f7657e78031f6f9a3ac4049c2f
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-17 10:01:27 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - DoHardThings
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+	i = 0;
+	while (i < 100)
+	{
+		j = (i / 10) % 10; /*tens*/
+		k = i / 100; /*unit*/
+		if (j < k)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			if (i < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
