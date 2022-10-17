a0fd74c75cf0343c4f2e2eb7b1953e4a815587a2
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-17 09:11:43 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  *
  * Return:zero if no error, Non-zero value if errors.
 */
- int main(void)
+int main(void)
 {
 	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-	printf("Size of an int: %id bytes(s)\n", sizeof(int));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld bytes(s)\n", sizeof(float));
