2d8f24ac36be5c5f55f5ba73a19dac575c9af9e8
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-18 03:50:28 -0700

    tasl1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * main - DoHardThings
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++);
+		_putchar(letter);	
+
+		_putchar('\n')
+}
