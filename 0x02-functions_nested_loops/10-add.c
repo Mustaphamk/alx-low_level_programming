c70bafa074cab216dcded35a67d4d5dfa07194ab
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-18 22:09:57 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add tow integer
+ * @num1: the first int to be added
+ * @num2: the second int to be added
+ *
+ * Return: the result of addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
