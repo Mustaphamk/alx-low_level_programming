a30bbb04be6f2f268c34becad306855bf6ec3b78
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-19 13:22:39 -0700

    task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0, count < 50, count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
@@ -22,7 +22,7 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(', ');
+			printf(", ");
 	}
 	return (0);
 }
