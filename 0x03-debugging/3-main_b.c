c0e44fb1c7d7404456cd3ea87f7fa5832837638d
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-10-20 13:37:20 -0700

    task3

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - take leap year into account
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 02;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
