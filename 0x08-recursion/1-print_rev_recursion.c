e842e8cbb953282290dfa4a0ff5e72b3d5cb51d4
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-11-10 14:43:54 -0800

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,21 +3,7 @@
 /**
  * _print_rev_recursion - reverse a string
  * @s: string
- *
-void _print_rev_recursion(char *s)
-{
-	int i = 0, lenght = 1, temp;
-
-	if (i > lenght / 2)
-	{
-		temp = s[i];
-		s[i] = s[lenght - i - 1];
-		s[lenght - i - 1] = temp;
-		i++;
-
-		_print_rev_recursion(s);
-	}
-}*/
+ */
 void _print_rev_recursion(char *s)
 
 {
