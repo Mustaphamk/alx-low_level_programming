f26a95e47883563ce8dd98c9ea7fc937354b3fc7
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-11-16 13:48:41 -0800

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,17 +10,13 @@ int _sqrt_recursion(int n)
 {
 	int c = 0;
 
-	if (n > 1)
+	if (n => 1)
 	{
 		return (n = c * _sqrt_recursion(c - 1));
 	}
 	else if (n < 1)
 	{
 		return (-1);
-	}
-	else if (n == 16777216)
-	{
-		return (4096);
 	}
 	else
 		return (1);
