fc1bee713b0a74ba76c08b2be7130066570fcf76
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-12-04 05:39:10 -0800

    tasi1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - count the num of arg passed unto
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
