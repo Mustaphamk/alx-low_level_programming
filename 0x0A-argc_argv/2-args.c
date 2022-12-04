56d54a355f76f61a4f5a7f515898076fa67abc66
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-12-04 14:30:57 -0800

    task3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	int c;
+
+	for (argv = 0; argv < c; argv++)
+	{
+		printf("%s\n", argv[c]);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv [2]);
+	printf("%d\n", mul);
+	return (0);
+}
