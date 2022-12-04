297820e90b01da36d7db4f5381a24f97d1a5d1f6
Muhammadmk <muhammadmustaphak321@gmail.com>
2022-12-04 05:05:04 -0800

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print my name
+ * @argc: arg counts
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
